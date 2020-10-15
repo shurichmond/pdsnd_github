1623b1639da4fcd6f43194f245fa84ec17d05104
shurichmond <shurichmond85@gmail.com>
2020-10-15 02:31:15 +0100

    Updated comment in time_filters function

--- bikeshare.py
+++ bikeshare.py
@@ -141,7 +141,7 @@ def time_filters(df, time, month, week_day, md):
         month = months.index(month) + 1
         df = df[df['month'] == month]
 
-    #Filter by day of week
+    #Filter week day
     if time == 'day_of_week':
         days = ['Monday', 'Tuesday',
         'Wednesday', 'Thursday',
